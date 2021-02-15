c13d8625a91fd1b9b2f7dbe8817095dd5d2e128e
Srie Teja <srit1505@gmail.com>
2021-02-15 23:19:24 +0530

    backend final

---
+++ install.sh
@@ -0,0 +1,31 @@
+# Install Docker
+
+# curl -fsSL https://get.docker.com -o get-docker.sh
+
+# chmod +x ./get-docker.sh
+
+# ./get-docker.sh
+
+# Install MySQL
+
+apt-get update -y
+
+apt-get install mysql-client mysql-server -y
+
+# Maven Installation
+
+apt install gradle -y
+
+# Java installation
+
+apt install default-jdk -y
+
+
+# Currently in root folder of the project
+cd XMeme-Backend
+
+mvn clean install
+
+cp target/XMeme-0.0.1-SNAPSHOT.jar XMeme.jar
+
+cd ../
